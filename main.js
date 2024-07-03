9b0429fbe8689ad31804fef5c4e1810f98f693cd
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-07-03 18:19:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// define variables
+var alienColor = "green";
+// using IF and ELSE-IF statement
+if (alienColor === "green") {
+    console.log("you just earned 5 points");
+}
+else if (alienColor === "yellow") {
+    console.log("you just earned 10 points");
+}
+else if (alienColor === " red") {
+    console.log("you just earnes 15 points");
+}
+// version 2
+// define variables
+var alienColor2 = "yellow";
+// using IF and ELSE-IF statement
+if (alienColor2 === "green") {
+    console.log("you just earned 5 points");
+}
+else if (alienColor2 === "yellow") {
+    console.log("you just earned 10 points");
+}
+else if (alienColor2 === " red") {
+    console.log("you just earnes 15 points");
+}
+// version 3
+// define variables
+var alienColor3 = "red";
+// using IF and ELSE-IF statement
+if (alienColor3 === "green") {
+    console.log("you just earned 5 points");
+}
+else if (alienColor3 === "yellow") {
+    console.log("you just earned 10 points");
+}
+else if (alienColor3 === "red") {
+    console.log("you just earnes 15 points");
+}
---
+++ main.ts
@@ -0,0 +1,47 @@
+// define variables
+let alienColor = "green";
+
+// using IF and ELSE-IF statement
+if(alienColor === "green"){
+    console.log("you just earned 5 points");
+}
+else if (alienColor === "yellow") {
+    console.log("you just earned 10 points");
+}
+else if(alienColor === " red"){
+    console.log("you just earnes 15 points");
+}
+
+// version 2
+
+// define variables
+let alienColor2 = "yellow";
+
+// using IF and ELSE-IF statement
+if(alienColor2 === "green"){
+    console.log("you just earned 5 points");
+}
+else if (alienColor2 === "yellow") {
+    console.log("you just earned 10 points");
+}
+else if(alienColor2 === " red"){
+    console.log("you just earnes 15 points");
+}
+
+// version 3
+// define variables
+let alienColor3 = "red";
+
+// using IF and ELSE-IF statement
+if(alienColor3 === "green"){
+    console.log("you just earned 5 points");
+}
+else if (alienColor3 === "yellow") {
+    console.log("you just earned 10 points");
+}
+else if(alienColor3 === "red"){
+    console.log("you just earnes 15 points");
+}
+
+
+
